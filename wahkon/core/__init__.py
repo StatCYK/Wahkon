fe708f868b9b98a269b4a2bcf3ebfd9a622650c3
Yongkai Chen <70113196+StatCYK@users.noreply.github.com>
2026-06-29 13:56:44 -0400

    Delete wahkon directory

--- wahkon/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-wahkon.core -- Low-level building blocks for the Wahkon network.
-
-Provides Gaussian kernel utilities and the WKN layer.
-"""
-
-from .spline import SS_batch, SS_coef2curve, SS_curve2coef
-from .layer import WKNLayer
